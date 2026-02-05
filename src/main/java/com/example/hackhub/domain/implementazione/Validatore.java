99f9fc733337c5021f04da80c039e7a3c1170e25
giagi <giadabranchesi2@gmail.com>
2026-02-05 19:17:00 +0100

    Creazione del diagramma delle classi di analisi della seconda iterazione

--- src/main/java/com/example/hackhub/domain/implementazione/Validatore.java
+++ src/main/java/com/example/hackhub/domain/implementazione/Validatore.java
@@ -4,7 +4,6 @@ import com.example.hackhub.repository.RepositoryHackathon;
 import com.example.hackhub.repository.RepositoryTeam;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 
 @Service
