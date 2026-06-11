04acf0b5e2fc3cc96dc100ed66868a5d28af1e79
TLCompanion <giadabranchesi2@gmail.com>
2026-06-11 22:04:08 +0200

    Fixed Visualizza Team

--- hackhub-frontend/src/pages/Dashboard.jsx
+++ hackhub-frontend/src/pages/Dashboard.jsx
@@ -106,7 +106,7 @@ export default function Dashboard(){
     useEffect(() => {
     const token = localStorage.getItem("token");
 
-    fetch("${API_URL}/team", {
+    fetch(`${API_URL}/team`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
